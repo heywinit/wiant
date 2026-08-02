bdc497bb84629ac6342d1517cca02e78d80d44df
Vinesh Rajpurohit <heywinit@gmail.com>
2026-08-02 18:10:28 +0530

    chore: using pooler for db connections & main.go cleanup
    adds pooling connection functionality with the help of
    pgxpool & a conn check after initializing the pool
    
    also rearranges things & some misc changes in main.go for
    cleanliness

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -11,69 +11,62 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/jackc/pgx/v5"
-
+	"github.com/heywinit/wiant/server/config"
 	wiantMiddleware "github.com/heywinit/wiant/server/internal/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	ctx, stop := signal.NotifyContext(
-		context.Background(),
-		os.Interrupt,
-		syscall.SIGTERM,
-	)
-	defer stop()
-
-	handler := prepareHandler(logger)
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: handler,
-	}
-
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-
+	cfg, err := config.Load()
 	if err != nil {
-		logger.Info("unable to connect to database", "err" , err)
+		logger.Error("invalid configuration", "error", err)
 		os.Exit(1)
 	}
-	defer conn.Close(ctx)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	pool, err := pgxpool.New(ctx, cfg.DatabaseURL)
+	if err != nil {
+		logger.Error("unable to create database pool", "error", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	if err = pool.Ping(ctx); err != nil {
+		logger.Error("unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+
+	server := &http.Server{Addr: cfg.Address, Handler: prepareHandler(logger), ReadHeaderTimeout: 10 * time.Second}
 
 	go func() {
-		logger.Info("started http server", "port", 3000)
-
-		err := http.ListenAndServe(":3000", handler)
-
-		if err != nil {
-			logger.Error("error while trying to start the server", "error", err.Error())
+		logger.Info("started http server", "address", cfg.Address)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("http server failed", "error", err)
+			stop()
 		}
 	}()
 
-	<-ctx.Done();
-	stop();
+	<-ctx.Done()
 
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel();
-
+	defer cancel()
 	if err := server.Shutdown(shutdownCtx); err != nil {
 		logger.Error("failed to gracefully shut down server", "error", err)
 	}
 }
 
-
 func prepareHandler(logger *slog.Logger) http.Handler {
 	r := chi.NewRouter()
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.ClientIPFromRemoteAddr)
-	r.Use(wiantMiddleware.RequestLogger(logger))
-	r.Use(middleware.Recoverer)
-
+	r.Use(middleware.RequestID, middleware.ClientIPFromRemoteAddr, wiantMiddleware.RequestLogger(logger), middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("wiant"))
+	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = w.Write([]byte("wiant"))
 	})
 
 	return r
--- server/go.mod
+++ server/go.mod
@@ -3,15 +3,23 @@ module github.com/heywinit/wiant/server
 go 1.26.3
 
 require (
+	github.com/coreos/go-oidc/v3 v3.20.0
+	github.com/go-chi/chi/v5 v5.3.1
+	github.com/jackc/pgx/v5 v5.10.0
+	golang.org/x/crypto v0.54.0
+	golang.org/x/oauth2 v0.36.0
+	golang.org/x/text v0.40.0
+)
+
+require (
 	github.com/dprotaso/go-yit v0.0.0-20220510233725-9ba8df137936 // indirect
 	github.com/getkin/kin-openapi v0.142.0 // indirect
 	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.4 // indirect
 	github.com/go-openapi/jsonpointer v0.23.1 // indirect
 	github.com/go-openapi/swag/jsonname v0.26.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.10.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/oapi-codegen/oapi-codegen/v2 v2.8.0 // indirect
 	github.com/oasdiff/yaml v0.1.1 // indirect
@@ -23,7 +31,7 @@ require (
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/mod v0.38.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
-	golang.org/x/text v0.40.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/tools v0.48.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
